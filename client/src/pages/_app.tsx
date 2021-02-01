ae597f996732c22463a6197bd036ba1f7edddfe7
khenryjoe <joseph.ketterer@gmail.com>
2021-02-01 18:30:06 +0000

    subs

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -7,21 +7,41 @@ import { useRouter } from "next/router";
 import {AuthProvider} from '../context/auth'
 import "../styles/tailwind.css";
 import "../styles/icons/style.css";
-
+import { SWRConfig } from 'swr'
+import axios from "axios";
 
 Axios.defaults.baseURL = "http://localhost:5000/api";
 Axios.defaults.withCredentials = true;
+
+const fetcher = async (url: string) => {
+  try {
+    const res = await axios.get(url)
+    return res.data
+    
+  } catch (err) {
+    throw err.response.data
+    
+  }
+}
+
 function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
   const authRoutes = ["/register", "/login"];
   const authRoute = authRoutes.includes(pathname);
   return (
+    <SWRConfig value={{
+      fetcher,
+      dedupingInterval: 10000
+    }}>
     <AuthProvider>
     
       {!authRoute && <Navbar />}
+      <div className={authRoute ? '' : 'pt-12'}>
       <Component {...pageProps} />
+      </div>
     
     </AuthProvider>
+    </SWRConfig>
   );
 }
 
